43c4209745c6cb218bfd7f2385c5224b06e6ba96
T3H1GH <76569372+T3H1GH@users.noreply.github.com>
2021-03-05 16:21:37 -0300

    Update bruteforce02.js

--- bruteforce02.js
+++ bruteforce02.js
@@ -8,30 +8,37 @@ function quebrar(senha) {     // Aqui começa a mágica!
 	let tamanho = key.length     // Essa é a quantidade de caracteres da senha
 	var arrkey = []              // Vamos armazenar os caracteres encontrados com o bruteforce aqui
 	console.log(`Sua senha tem ${tamanho} caracteres`)
+
 	for (s in key) {            // Enquanto s(contador) estiver em key(senha no formato de array)
-	  let cont = Number(s) + 1  // Criei outro contador aqui pois o "s" vai começar no 0, quero começar do 1
+
+	  let cont = Number(s) + 1  // Cria outro contador pois o "s" vai começar no 0, quero começar do 
 	  // O "s" está como strig por isso passei ele para número antes de somar
+
 	  for (c in caracteres) {   // Enquanto c(mais um contador) estiver em caracteres
+
 			console.log(caracteres[c])   // Isso vai exibir os caracteres um a um até o if abaixo se tornar verdadeiro
+			
 			if (caracteres[c] == key[s]) {    // Se o caractere na posição "c" for igual ao caractere da chave na posição "s"
-			  // A cada loop ele vai encontrar um caractere da senha
+			 	// A cada loop ele vai encontrar um caractere da senha
+
 				arrkey.push(caracteres[c])      // Armazena o caractere achado em arrkey
 				console.log(`caractere ${cont} encontrado --> ${arrkey[s]}`)   // Aqui eu ultilizo aquele contador que criei mais acima
-				break         // Depois que o if se torna verdadeiro ele para o loop (secundário)
+				break         // Depois que o if se torna verdadeiro ele para esse loop
 			}
 		}
 	  
         if (cont == tamanho) {           
-          // Se o contador for igual ao tamanho da senha ele retorna o valor encontrado no console.
+          	// Se o contador for igual ao tamanho da senha ele retorna o valor encontrado no console.
 	        console.log(`Encontrado!\nSua sehha é ${arrkey.join('')}`)
 		}
+		// Depois ele repete isso até achar a senha
 	}
     return arrkey.join('')      // A função vai retornar a senha encontrada em string
 }
 console.log(quebrar(chave))
 
-// Obrigado por ler até aqui!
 // Essa função vai quebrar strings de 
 // qualquer tamanho com tanto q esteja com 
-// os caracteres listados em "abc", senão 
-// ele vai dar um loop infinito.
+// os caracteres listados em "abc"
+
+// Obrigado por ler até aqui!
