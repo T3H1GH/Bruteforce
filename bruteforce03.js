8e3642aecd4218a349a738b0e03a9fab188199be
T3H1GH <76569372+T3H1GH@users.noreply.github.com>
2021-03-05 16:34:41 -0300

    Update bruteforce03.js

--- bruteforce03.js
+++
@@ -1,8 +0,0 @@
-senha = "aguia"
-
-for(i=parseInt("aaaa", 36);i++<=parseInt("zzzzzz", 36);console.log(i.toString(36))) {
-    if (i == senha) {
-        console.log('Encontrado!')
-        break
-    }
-}
