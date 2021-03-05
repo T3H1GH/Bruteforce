f15d058e3eb8004105cc519dd600664f8400a61f
T3H1GH <76569372+T3H1GH@users.noreply.github.com>
2021-03-04 21:39:10 -0300

    Update bruteforce01.js

--- bruteforce01.js
+++ bruteforce01.js
@@ -1,38 +1,64 @@
-let chave = "ca"
+let chave = "cabo"
 let abc = "aeioubcdfghjklmnpqrstvwxyz"
 let res = abc.split("")							// Retorna um array
 
 function quebrar(senha) {
-	let key = senha.split("")					// Retorna um array
+	let key = senha.split("")					// Transforma a chave em array
 	let tamanho = key.length
 	if (tamanho >= 1) {
 		for (let c = 0; c < res.length; c++) {
 			console.log(res[c])
 			if (res[c] == key[0]) {
 				var l1 = res[c]
-				console.log(`Primeira letra ${l1}`)
+				console.log(`Primeiro caractere --> ${l1}`)
 				if (tamanho == 1) {
 					console.log(`Encontrado!\nSua sehha é ${l1}`)
 				}
 				break
 			}
-			
 		}
-		if (tamanho >= 2) {
-			for (let c = 0; c < res.length; c++) {
-				console.log(res[c])
-				if (res[c] == key[1]) {
-					var l2 = res[c]
-					let suaSenha = l1 + l2 
-					if (tamanho == 2) {
-						console.log(`Encontrado!\nSua sehha é ${suaSenha}`)
-					}
-					break
+	}
+	if (tamanho >= 2) {
+		for (let c = 0; c < res.length; c++) {
+			console.log(res[c])
+			if (res[c] == key[1]) {
+				var l2 = res[c]
+				console.log(`Segundo caractere --> ${l2}`)
+				let suaSenha = l1 + l2 
+				if (tamanho == 2) {
+					console.log(`Encontrado!\nSua sehha é ${suaSenha}`)
 				}
-				}
-
+				break
+			}
 		}
-
+	}
+	if (tamanho >= 3) {
+		for (let c = 0; c < res.length; c++) {
+			console.log(res[c])
+			if (res[c] == key[2]) {
+				var l3 = res[c]
+				console.log(`Terceiro caractere --> ${l3}`)
+				let suaSenha = l1 + l2 + l3
+				if (tamanho == 3) {
+					console.log(`Encontrado!\nSua sehha é ${suaSenha}`)
+				}
+				break
+			}
+		}
+	}
+	if (tamanho >= 4) {
+		for (let c = 0; c < res.length; c++) {
+			console.log(res[c])
+			if (res[c] == key[3]) {
+				var l4 = res[c]
+				console.log(`Quarto caractere --> ${l4}`)
+				let suaSenha = l1 + l2 
+				if (tamanho == 4) {
+					console.log(`Encontrado!\nSua sehha é ${suaSenha}`)
+				}
+				break
+			}
+		}
 	}
 
 	return ''
@@ -41,25 +67,3 @@ function quebrar(senha) {
 
 
 console.log(quebrar(chave))
-
-
-
-
-
-
-
-/*
-function createSilaba() {
-    if (silabas.length < 105) {
-        silabas.push(consoantes[cpos] + vogais[pos])
-		pos ++
-		if (pos == 5) {
-		    cpos++
-			pos = 0
-		}
-		createSilaba()
-
-    } 
-}
-createSilaba()
-*/
