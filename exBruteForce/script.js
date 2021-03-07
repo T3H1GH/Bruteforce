0936deed21129135a6955e7df8206d4a92f55269
T3H1GH <76569372+T3H1GH@users.noreply.github.com>
2021-03-07 11:53:31 -0300

    Atualizações
    CSS atualizado(melhorias de interface)
    Melhorias em script.js

--- exBruteForce/script.js
+++ exBruteForce/script.js
@@ -4,69 +4,73 @@ let abc = document.getElementById('abc')
 
 
 function clicar() {
-    res.innerHTML = ``
-    let abc = "aeioubcdfghjklmnpqrstvwxyzAEIOUBCDFGHJKLMNPQRSTVWXYZ1234567890"
-    let caracteres = abc.split("")
-    let key = senha.value.split("")
-    let tamanho = key.length
-    var arrkey = []
 
+    if (senha.value.length == 0) {
+        alert('Digite uma senha')
+    } else {
+        res.innerHTML = ``
+        let abc = "aeioubcdfghjklmnpqrstvwxyzAEIOUBCDFGHJKLMNPQRSTVWXYZ1234567890!@#$%¨&*()_+-= "
+        let caracteres = abc.split("")
+        let key = senha.value.split("")
+        let tamanho = key.length
+        var arrkey = []
     
-    let select01 = document.createElement('select')
-    let select02 = document.createElement('select')
-    
-    select01.size = 10
-    select01.style.width = '200px'
-    select01.style.marginLeft = '15px'
-    select01.style.marginTop = '20px'
-    
-    
-    select02.size = 10
-    select02.style.width = '200px'
-    select02.style.marginLeft = '15px'
-    select02.style.marginTop = '20px'
-
-    res.appendChild(select01)
-    res.appendChild(select02)
-
- 
-    criarOption(2, "Caracteres encontrados")
-    criarOption(1, `Sua senha tem ${tamanho} caracteres`)
-    
-
-    function criarOption(sel, texto) {
-        if (sel == 1) {
-            item = document.createElement('option')
-            item.text += texto
-            select01.appendChild(item)
-        } else if (sel == 2) {
-            item = document.createElement('option')
-            item.text += texto
-            select02.appendChild(item)
-        }
-    }
-    
-
-    for (s in key) {            
-        let cont = Number(s) + 1  
         
-        for (c in caracteres) {   
-              criarOption(2, caracteres[c])  
-              
-            if (caracteres[c] == key[s]) {    
-                arrkey.push(caracteres[c])   
-                criarOption(1, `${arrkey[s]}`)  
-                criarOption(2, `Caractere ${cont} encontrado --> ${arrkey[s]}`)   
-                break         
+        let select01 = document.createElement('select')
+        let select02 = document.createElement('select')
+        
+        select01.size = 10
+        select02.size = 10
+    
+        res.appendChild(select01)
+        res.appendChild(select02)
+    
+     
+        criarOption(1, "Caracteres encontrados:")
+        criarOption(2, `Sua senha tem ${tamanho} caracteres`)
+
+        
+        function criarOption(sel, texto) {
+            item = document.createElement('option')
+            item.text += texto
+            if (sel == 1) {
+                select01.appendChild(item)
+            } else if (sel == 2) {
+                select02.appendChild(item)
             }
         }
-        
-        if (cont == tamanho) {           
-            criarOption(2, `Terminado!`)
-            criarOption(1, `Sua sehha é ${arrkey.join('')}`)
+    
+    
+        for (s in key) {            
+            let cont = Number(s) + 1  
+            
+            for (c in caracteres) {   
+                criarOption(1, caracteres[c])  
+
+                if (caracteres[c] == key[s]) { 
+                    arrkey.push(caracteres[c])   
+                    criarOption(2, `${arrkey[s]}`)  
+                    criarOption(1, `Caractere ${cont} encontrado --> ${arrkey[s]}`)   
+                    if (caracteres.length == Number(c) + 1) {
+                        if (caracteres[c] == ' ') {
+                        } else {
+                            criarOption(1, `Caractere não encontrado`)
+                            arrkey.push(undefined)
+                        }
+                    }
+                    break         
+                }
+            }
+            
+            if (cont == tamanho) {           
+                criarOption(1, `Terminado!`)
+                criarOption(2, `Sua sehha é ${arrkey.join('')}`)
+            }
         }
+        if (tamanho != arrkey.length) {
+            let diferença = Number(tamanho) - Number(arrkey.length)
+            criarOption(2, `${diferença} Caractere não encontrado`)
+        }
+        senha.focus()
     }
 }
-
-
-
