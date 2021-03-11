e059f6d2390ae4187b31a24d6225b3244c1be789
T3H1GH <76569372+T3H1GH@users.noreply.github.com>
2021-03-11 18:24:48 -0300

    Update bruteforce03.js

--- bruteforce03.js
+++ bruteforce03.js
@@ -1,1 +1,24 @@
+let senha = "bsa"
+let abc = "aeioubcdfghjklmnpqrstvwxyz"  //AaEeIiOoUuBbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz1234567890!@#$%¨&*()_+-= 
+let caracteres = abc.split("")
 
+
+let key
+
+for (let c in caracteres) {
+    let l1 = caracteres[c]
+    if (key == senha) {break}
+    for (let c in caracteres) {
+        let l2 = caracteres[c]
+        if (key == senha) {break}
+        for (let c in caracteres) {
+            let l3 = caracteres[c]
+            key = l1 + l2 + l3
+            console.log(key)
+            if (key == senha) {
+                console.log("Encontrado!")
+                break
+            }
+        }
+    }
+}
